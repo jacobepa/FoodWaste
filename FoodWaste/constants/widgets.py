6f78c25db924c119a61f448421ad5cff1427c956
Daniel L. Young, Ph.D <young.daniel@epa.gov>
2019-10-23 06:45:23 -0400

    Minor pep257 fixes...

--- FoodWaste/constants/widgets.py
+++ FoodWaste/constants/widgets.py
@@ -15,13 +15,16 @@ from django import forms
 # https://stackoverflow.com/questions/24783275/django-form-with-choices-but-also-with-freetext-option
 class ListTextWidget(forms.TextInput):
     """Custom widget to allow a user to select a dropdown option, or type in a custom option."""
+
     def __init__(self, data_list, name, *args, **kwargs):
+        """Add docstring."""
         super(ListTextWidget, self).__init__(*args, **kwargs)
         self._name = name
         self._list = data_list
         self.attrs.update({'list':'list__%s' % self._name})
 
     def render(self, name, value, attrs=None, renderer=None):
+        """Add docstring."""
         text_html = super(ListTextWidget, self).render(name, value, attrs=attrs)
         data_list = '<datalist id="list__%s">' % self._name
         for item in self._list:
@@ -32,5 +35,5 @@ class ListTextWidget(forms.TextInput):
 
 
 def strip_non_numerals(param_string):
-    """Given a string (such as a phone number) strip all non-numerics"""
+    """Given a string (such as a phone number) strip all non-numerics."""
     return ''.join(c for c in param_string if c.isdigit())
