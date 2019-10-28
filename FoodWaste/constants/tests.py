cdaf0354c4fa7de0bbd71b0eb874f4f366b1b899
Daniel L. Young, Ph.D <young.daniel@epa.gov>
2019-10-28 09:57:48 -0400

    Added some initial 'tests', updated additional extensions/packages in requirements.txt, added 'Test Module.'

--- FoodWaste/constants/tests.py
+++ FoodWaste/constants/tests.py
@@ -4,8 +4,118 @@
 # young.daniel@epa.gov
 
 """
-Unit tests for constants tests.
+This file houses test cases for the constants module.
 
 Available functions:
-- None. TBD.
+- None
 """
+
+from django.test import TestCase
+from django.core.mail import EmailMultiAlternatives
+from constants.utils import split_email_list, is_epa_email, non_epa_email_message, create_qt_email_message, xstr, \
+    sort_rap_numbers, get_rap_fields, is_float
+
+class TestUtils(TestCase):
+    """Test utils."""
+
+    def test_split_email_list_pass_one(self):
+        """Runs the char split on an email that will be equal."""
+        self.assertEqual(split_email_list("t;e,s\tt@t|est.com"), ['t', 'e', 's', 't@t', 'est.com'])
+
+    def test_split_email_list_fail_one(self):
+        """Runs the char split on an email that will not be equal."""
+        self.assertNotEqual(split_email_list("t;e,s\tt@t|est.com"), ['t', 'e', 's', 't@test.com'])
+
+    def test_is_epa_email_pass_one(self):
+        """Runs the test to check if an email address is in the right format to be an epa email address."""
+        self.assertEqual(is_epa_email("test@epa.gov"), True)
+
+    def test_is_epa_email_fail_one(self):
+        """Runs the test to check if an email address is in the right format to be an epa email address."""
+        self.assertEqual(is_epa_email("test@test.com"), False)
+
+    def test_is_epa_email_fail_two(self):
+        """Runs the test to check if an email address is in the right format to be an epa email address."""
+        self.assertEqual(is_epa_email("test@test.gov"), False)
+
+    def test_is_epa_email_fail_three(self):
+        """Runs the test to check if an email address is in the right format to be an epa email address."""
+        self.assertEqual(is_epa_email("test@epa.com"), False)
+
+    def test_is_epa_email_fail_seven(self):
+        """Runs the test to check if an email address is in the right format to be an epa email address."""
+        self.assertEqual(is_epa_email("test@Aepa.gov"), False)
+
+    def test_is_epa_email_fail_eight(self):
+        """Runs the test to check if an email address is in the right format to be an epa email address."""
+        self.assertEqual(is_epa_email("test@repa.govR"), False)
+
+    def test_is_epa_email_fail_nine(self):
+        """Runs the test to check if an email address is in the right format to be an epa email address."""
+        self.assertEqual(is_epa_email("test@4epa.gov"), False)
+
+    def test_non_epa_email_message_fail_one(self):
+        """Runs the test to check if an email address is not EPA that it sends the correct message."""
+        self.assertIn(
+            """Email list may only contain @epa.gov addresses.""", non_epa_email_message("test@test.com"), msg=None)
+
+    def test_create_qt_email_message(self):
+        """Returns EmailMultiAlternatives object."""
+        to_emails = ["testTo@test.com"]
+        self.assertIsInstance(
+            create_qt_email_message("email Subject", "text content", "testFrom@test.com", to_emails, None, None),
+            EmailMultiAlternatives, msg=None)
+
+    def test_xstr_one(self):
+        """Test the method that Checks for and replaces None objects with empty strings."""
+        self.assertEqual(xstr(None), "")
+
+    def test_xstr_two(self):
+        """Test the method that Checks for and replaces None objects with empty strings."""
+        self.assertEqual(xstr("test"), "test")
+
+    def test_sort_rap_numbers(self):
+        """Test the sort rap numbers."""
+        raplist = {"15.42", "15.45", "12.12"}
+        results = sort_rap_numbers(raplist)
+        self.assertEqual(results, ["12.12", "15.42", "15.45"])
+
+    def test_sort_rap_numbers_fail(self):
+        """Test the sort rap numbers."""
+        raplist = {"15.42", "test", "12.12"}
+        results = sort_rap_numbers(raplist)
+        # print(results)
+        self.assertEqual(results, ["12.12", "15.42", "test"])
+
+    def test_get_rap_fields(self):
+        """Test the get rap fields with form."""
+        fields = "form"
+        results = get_rap_fields(fields)
+        # print(results)
+        self.assertEqual(results, [
+            ['ace', 'ace_rap_numbers'], ['css', 'css_rap_numbers'],
+            ['sswr', 'sswr_rap_numbers'], ['hhra', 'hhra_rap_numbers'],
+            ['hsrp', 'hsrp_rap_numbers'], ['hsre', 'hsrp_rap_extensions'],
+            ['shc', 'shc_rap_numbers']])
+
+    def test_get_rap_fields_two(self):
+        """Test the get rap fields without form."""
+        fields = "notform"
+        results = get_rap_fields(fields)
+        # print(results)
+        self.assertEqual(results, ['ace_rap_numbers', 'css_rap_numbers', 'sswr_rap_numbers', 'hhra_rap_numbers',
+                                   'hsrp_rap_numbers', 'hsrp_rap_extensions', 'shc_rap_numbers'])
+
+    def test_is_float_one(self):
+        """Test the is float method with a float."""
+        val_str = "1.2"
+        results = is_float(val_str)
+        # print(results)
+        self.assertEqual(results, True)
+
+    def test_is_float_two(self):
+        """Test the is float method with a char string."""
+        val_str = "notAFloat"
+        results = is_float(val_str)
+        # print(results)
+        self.assertEqual(results, False)
---
+++ FoodWaste/constants/utils.py
@@ -0,0 +1,134 @@
+# utils.py (constants)
+# !/usr/bin/env python3
+# coding=utf-8
+# young.daniel@epa.gov
+
+"""
+Utility methods for the entire project.
+
+Available functions:
+- split_email_list
+- is_epa_email
+- non_epa_email_message
+- create_qt_email_message
+- get_rap_fields
+- sort_rap_numbers
+- xstr
+"""
+
+from operator import itemgetter
+from django.core.mail import EmailMultiAlternatives
+from django.conf import settings
+
+
+# Email Utility functions
+def split_email_list(email_list):
+    """Replace all defined delimiter characters with spaces."""
+    delimiters = [';', ',', '\t', '|']
+    for delim in delimiters:
+        email_list = email_list.replace(delim, ' ')  # replace delims with a space
+
+    return email_list.split()
+
+
+def is_epa_email(email):
+    """Check if an email is of the EPA domain 'epa.gov'."""
+    email = email.strip().lower()
+    epa = '@epa.gov'
+    if epa not in email:  # initial, does it contain @epa.gov
+        return False
+
+    ending = email[email.index(epa) + len(epa):]  # email contains @epa.gov
+    # Preventing alphanumeric and '.' to prevent @epa.gov_bad_domain.com...
+    # who would do this anyway? Users.
+    illegal_end_chars = r"[a-z][A-Z][0-9]\."
+
+    if any(elem in illegal_end_chars for elem in ending):  # checks if string has any of the illgal chars
+        return False
+    # print(ending)
+    # print(illegal_end_chars)
+    return True  # if we pass, it must be epa right?
+
+
+def non_epa_email_message(emails):
+    """
+    Lets the user know which emails are not EPA.
+
+    And that they must be sent manually.
+    """
+    return "Email list may only contain @epa.gov addresses. Please sendnon-EPA emails directly from Outlook. \
+            Offending email(s): " + ', '.join(emails)
+
+
+def create_qt_email_message(email_subject, text_content, from_email, to_emails,
+                            carbon_copy=None, blind_carbon_copy=None):
+    """
+    Modify email content.
+
+    To have a disclaimer at top and adds blind_carbon_copy
+    for monitoring that email functionality is working. Returns
+    EmailMultiAlternatives object.
+    """
+    if settings.BCC_GSC_EMAIL:
+        if blind_carbon_copy:
+            blind_carbon_copy.append(settings.BCC_GSC_EMAIL)
+        else:
+            blind_carbon_copy = [settings.BCC_GSC_EMAIL]
+
+    html_content = settings.EMAIL_DISCLAIMER + text_content.replace("\r\n", "<br>").replace("\n", "<br>")
+    text_content_modified = settings.EMAIL_DISCLAIMER_PLAIN + "\r\n\r\n" + text_content
+
+    the_email = EmailMultiAlternatives(
+        email_subject, text_content_modified, from_email, to_emails, cc=carbon_copy, bcc=blind_carbon_copy)
+    the_email.attach_alternative(html_content, "text/html")
+
+    return the_email
+
+
+# RAP Utility Functions
+def get_rap_fields(fields='default'):
+    """Return a list of rap fields."""
+    if fields == 'form':
+        return [
+            ['ace', 'ace_rap_numbers'], ['css', 'css_rap_numbers'],
+            ['sswr', 'sswr_rap_numbers'], ['hhra', 'hhra_rap_numbers'],
+            ['hsrp', 'hsrp_rap_numbers'], ['hsre', 'hsrp_rap_extensions'],
+            ['shc', 'shc_rap_numbers']]
+
+    return ['ace_rap_numbers', 'css_rap_numbers', 'sswr_rap_numbers', 'hhra_rap_numbers',
+            'hsrp_rap_numbers', 'hsrp_rap_extensions', 'shc_rap_numbers']
+
+
+def sort_rap_numbers(rap_list):
+    """Sorts the rap numbers first by it's first digit then alphanumerically."""
+    def _get_numeric(the_str):
+        try:
+            val = float(the_str.split('.')[0])  # e.g. the 16 in 16.2.2
+            return val
+        except ValueError:
+            return float("inf")  # woah if not numeric assign infinity e.g. like CIVA-2.5
+
+    arr = [(row, _get_numeric(row)) for row in rap_list]
+    return [row[0] for row in sorted(arr, key=itemgetter(1, 0))]
+
+
+def xstr(p_str):
+    """Check for and replaces None objects with empty strings."""
+    if p_str is None:
+        return ''
+    return str(p_str)
+
+
+def is_float(val_str):
+    """
+    Take a string and tries to parse it as a float.
+
+    If parse fails, returns false. Otherwise parse succeeds
+    and true is returned.
+    """
+    try:
+        float(val_str)
+    except ValueError:
+        return False
+    return True
+
