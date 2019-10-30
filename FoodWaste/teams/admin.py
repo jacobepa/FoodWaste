167d462e17ca2a85bc8cf7f82a68a4461442aaba
Daniel L. Young, Ph.D <young.daniel@epa.gov>
2019-10-30 11:47:07 -0400

    Fix team creation via admin portal.

--- FoodWaste/teams/admin.py
+++ FoodWaste/teams/admin.py
@@ -14,5 +14,22 @@ Available functions:
 from django.contrib import admin
 from teams.models import Team, TeamMembership
 
-admin.site.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    """Custom Admin class for managing Teams"""
+    model = Team
+    fields = ('name',)
+    readonly_fields = ('created_date', 'created_by', 'last_modified_date',
+                       'last_modified_by', 'members',)
+
+    def save_model(self, request, obj, form, change):
+        """Custom save method to attach created_by user"""
+        try:
+            temp_obj = obj.created_by
+        except:
+            obj.created_by = request.user
+        obj.last_modified_by = request.user
+        obj.save()
+
+admin.site.register(Team, TeamAdmin)
+
 admin.site.register(TeamMembership)
