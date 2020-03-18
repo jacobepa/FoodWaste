e5f5a507ee00b82ff96d4c7e6ace25f5a796a31c
Jacob S <jacob@gqc.com>
2020-03-18 15:43:36 -0400

    Move site_name into settings.py and fix/clean up several anchor (button) tags.

--- DataSearch/DataSearch/settings.py
+++ DataSearch/DataSearch/settings.py
@@ -23,6 +23,8 @@ import os
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+SITE_NAME = 'https://134.67.216.106/accounts/login'
+
 EMAIL_DISCLAIMER = '<p style="font-weight:bold;color:red">Please do not reply to this email. Thank you.</p>'
 EMAIL_DISCLAIMER_PLAIN = 'Please do not reply to this email. Thank you.'
 BCC_EMAIL = ''
