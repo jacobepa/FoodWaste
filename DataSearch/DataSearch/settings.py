e2687199594e885d1b730972b1eaa40f8eab0f50
jacobgqc <jacob@gqc.com>
2023-08-03 14:14:05 -0400

    Update settings, requirements, and navbar for saml2.

--- DataSearch/DataSearch/settings.py
+++ DataSearch/DataSearch/settings.py
@@ -123,12 +123,8 @@ WSGI_APPLICATION = 'DataSearch.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases.
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'datasearch',
-        'USER': 'postgres',
-        'PASSWORD': '____________________',
-        'HOST': '127.0.0.1',
-        'PORT': '5432'
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
